b377e9ee889d5950845909dff55b055d75c363ed
NomzNomz <calvin.reid@roxburylatin.org>
2024-04-17 22:31:03 -0400

    planning

---
+++ AutoFlipper.java
@@ -0,0 +1,13 @@
+public class AutoFlipper {
+    public static void main(String[] args) {
+        while(true){
+            //get recent auctions
+            //create auctioneditems objects
+            //compare prices
+            //buy anything with a 15% discrepancy - get ah id, create pastable string
+            //get sell prices
+            //post auction
+            //wait a second or two
+        }
+    }
+}

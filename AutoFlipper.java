8d1843bd2b468c211ced71d531eccb0e39c8f16f
NomzNomz <calvin.reid@roxburylatin.org>
2024-04-18 22:49:10 -0400

    Update AutoFlipper.java

--- AutoFlipper.java
+++ AutoFlipper.java
@@ -1,8 +1,15 @@
+import java.util.ArrayList;
+
 public class AutoFlipper {
     public static void main(String[] args) {
         while(true){
             //get recent auctions
             //create auctioneditems objects
+            ArrayList<AuctionedItem> mainArrayList = new ArrayList<>();
+
+            ApiCaller MainCaller = new ApiCaller("f43b2f7b-affd-4d71-b51b-a3ee3111657f");
+            mainArrayList = AuctionedItem.createAuctionedItemsFromApi(MainCaller.CallFinishedAuctions());
+
             //compare prices
             //buy anything with a 15% discrepancy - get ah id, create pastable string
             //get sell prices

36bc429785ce3fdaa4e3a04f10b80a8cbf869ff9
Nathan Zhang <nathan.zhang@roxburylatin.org>
2024-05-17 13:01:43 -0400

    data collection done

--- ApiPrinter.java
+++ ApiPrinter.java
@@ -8,27 +8,35 @@ public class ApiPrinter {
         StopWatch timer = new StopWatch();
 
         ApiCaller MainCaller = new ApiCaller("f43b2f7b-affd-4d71-b51b-a3ee3111657f");
-        test = WeaponArmor.createAuctionedItemsFromApi(MainCaller.CallFinishedAuctions(), true);
+        String finishedAuctions = "";
+        String temp = "";
 //        test2 = WeaponArmor.createAuctionedItemsFromApi(MainCaller.CallNewAuctions(), false);
         int count = 0;
+        StopWatch watch = new StopWatch();
+        watch.startWatch();
 
-
-        for(WeaponArmor item : test){
-            if(!item.getName().isEmpty()){
-                count++;
-                System.out.println(item.getName());
+        while(true){
+            while(watch.getTimeSeconds() < 8){}
+            finishedAuctions = MainCaller.CallFinishedAuctions();
+            if(!finishedAuctions.equals(temp)){
+                temp = finishedAuctions;
+                test = WeaponArmor.createAuctionedItemsFromApi(finishedAuctions, true);
+                for(WeaponArmor item : test){
+                    if(!item.getName().isEmpty()){
+//                    count++;
+//                    System.out.println(item.getName());
 //                System.out.println(item.getReasonableJSON());
-                System.out.println(item.numFPBS);
-                System.out.println("/viewauction " + item.getAuctionID());
-                System.out.println(item.apiData);
-                System.out.println(item.getStarLevel());
-                item.writeArrayListToCSV("output.csv");
-                System.out.println();
-                System.out.println();
+//                    System.out.println(item.numFPBS);
+//                    System.out.println("/viewauction " + item.getAuctionID());
+//                    System.out.println(item.apiData);
+//                    System.out.println(item.getStarLevel());
+                        item.writeArrayListToCSV("output.csv");
+//                    System.out.println();
+//                    System.out.println();
+                    }
+                }
             }
         }
-        System.out.println(count);
-        System.out.println(test.size());
 
     }
 }

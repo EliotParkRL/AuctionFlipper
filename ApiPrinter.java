a879df625011a9ec23c01a1a1d5c59a44f073702
Nathan Zhang <nathan.zhang@roxburylatin.org>
2024-05-17 00:38:04 -0400

    worked on adding the api info to an instance variable

--- ApiPrinter.java
+++ ApiPrinter.java
@@ -1,4 +1,3 @@
-import javax.xml.crypto.Data;
 import java.io.IOException;
 import java.util.ArrayList;
 
@@ -9,12 +8,12 @@ public class ApiPrinter {
         StopWatch timer = new StopWatch();
 
         ApiCaller MainCaller = new ApiCaller("f43b2f7b-affd-4d71-b51b-a3ee3111657f");
-//        test = WeaponArmor.createAuctionedItemsFromApi(MainCaller.CallFinishedAuctions(), true);
-        test2 = WeaponArmor.createAuctionedItemsFromApi(MainCaller.CallNewAuctions(), false);
+        test = WeaponArmor.createWeaponArmorFromApi(MainCaller.CallFinishedAuctions(), true);
+//        test2 = WeaponArmor.createWeaponArmorFromApi(MainCaller.CallNewAuctions(), false);
         timer.startWatch();
         int count = 0;
 
-        for(WeaponArmor item : test2){
+        for(WeaponArmor item : test){
 //          System.out.println(item.dumpJSON());
 //            System.out.println();
 //            System.out.println();
@@ -23,10 +22,18 @@ public class ApiPrinter {
 //                System.out.println(item.getName());
 //                System.out.println(item.getAuctionID());
 //            }
-            System.out.println(item.getReasonableJSON());
+//            System.out.println(item.getReasonableJSON());
+
             if(!item.getName().isEmpty()){
                 count += 1;
+                System.out.println(item.getName());
+                System.out.println(item.getReasonableJSON());
+                System.out.println("/viewauction " + item.getAuctionID());
+                System.out.println(item.apiData);
+                System.out.println();
+                System.out.println();
             }
+
 //            System.out.println(item.getAuctionPrice());
 //            System.out.println(item.getName());
 //            System.out.println(item.getAuctionID());
